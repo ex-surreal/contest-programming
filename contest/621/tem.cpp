e9f437a59427ca21493c0bab452cd3ceb5435fa3
Abdul Mumit <mamumit07@gmail.com>
2016-02-04 23:13:48 +0600

    621

---
+++ contest/621/tem.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define mem(a, v) memset(a, v, sizeof (a))
+#define pb push_back
+#define mp make_pair
+#define sz(x) int((x).size())
+#define x first
+#define y second
+#define all(a) (a).begin(), (a).end()
+#define rep(i, n) for(int i = 0; i < int(n); i ++)
+#define repi(i, a, n) for(int i = int(a); i < int(n); i ++)
+
+int main () {
+    ios_base::sync_with_stdio(false);
+    return 0;
+}
