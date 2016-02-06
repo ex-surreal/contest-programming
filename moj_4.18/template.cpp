cb163f2f3734c7c4eac8de7be7275af18548473b
Abdul Mumit <mamumit07@gmail.com>
2016-02-06 22:49:17 +0600

    Add moj

---
+++ moj_4.18/template.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define mem(a, v) memset(a, v, sizeof (a))
+#define x first
+#define y second
+#define all(a) (a).begin(), (a).end()
+#define mp make_pair
+#define pb push_back
+#define sz(x) int((x).size())
+#define rep(i, n) for (int i = 0; i < int(n); i ++)
+#define repi(i, a, n) for (int i = (a); i < int(n); i ++)
+#define repe(x, v) for (auto x: (v))
+
+class $CLASSNAME$ {
+public:
+    $RC$ $METHODNAME$($METHODPARMS$) {
+        $RC$ ret;
+        return ret;
+    }
+};
+
+$BEGINCUT$
+$TESTCODE$
+
+$DEFAULTMAIN$
+$ENDCUT$
