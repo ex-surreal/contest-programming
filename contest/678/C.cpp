d33efa32cd39932094c18259103b6fb42d1635d6
Abdul Mumit <mamumit07@gmail.com>
2016-06-15 15:50:31 +0600

    Add contest 678

---
+++ contest/678/C.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <map>
+#include <cmath>
+#include <set>
+#include <queue>
+#include <stack>
+#include <cstring>
+#include <ctime>
+#include <cstdlib>
+#include <cassert>
+
+using namespace std;
+
+#define mem(a, v) memset(a, v, sizeof (a))
+#define x first
+#define y second
+#define all(a) (a).begin(), (a).end()
+#define mp make_pair
+#define pb push_back
+#define sz(x) int((x).size())
+#define rep(i, n) for (int i = 0; i < int(n); i ++)
+#define repi(i, a, n) for (int i = (a); i < int(n); i ++)
+#define repe(x, v) for (auto x: (v))
+
+int main () {
+    std::ios_base::sync_with_stdio(false);
+    long long n, a, b, p, q;
+    cin >> n >> a >> b >> p >> q;
+    long long lcm = a*b/__gcd(a, b);
+    cout << p*(n/a-n/lcm)+q*(n/b-n/lcm)+max(p, q)*(n/lcm);
+    return 0;
+}
