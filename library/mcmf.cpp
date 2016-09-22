d8136e2368240f549fff15c76db2fc8e41ee3e8c
Abdul Mumit <mamumit07@gmail.com>
2016-09-22 14:52:47 +0600

    Change indentation of mcmf

--- library/mcmf.cpp
+++ library/mcmf.cpp
@@ -10,81 +10,81 @@ vector <int> L, pre, to;
 vector <int> cap, flow, cost;
 
 void init(int n) {
-    L = vector <int>(n, -1);
-    pre.clear();
-    to.clear();
-    cap.clear();
-    flow.clear();
-    cost.clear();
+  L = vector <int>(n, -1);
+  pre.clear();
+  to.clear();
+  cap.clear();
+  flow.clear();
+  cost.clear();
 }
 
 void add(int u, int v, int c = 1, int w = 0) {
-    to.push_back(v);
-    pre.push_back(L[u]);
-    cap.push_back(c);
-    cost.push_back(w);
-    flow.push_back(0);
-    L[u] = pre.size()-1;
+  to.push_back(v);
+  pre.push_back(L[u]);
+  cap.push_back(c);
+  cost.push_back(w);
+  flow.push_back(0);
+  L[u] = pre.size()-1;
 }
 
 void add_edge(int u, int v, int c = 1, int w = 0) {
-    add(u, v, c, w);
-    add(v, u, 0, -w);
+  add(u, v, c, w);
+  add(v, u, 0, -w);
 }
 
 bool augment(int s, int t, fc & ans) {
-    int n = L.size();
-    vector <int> D(n, inf), Q(n), F(n), parent(n, -1), edge(n);
-    vector <bool> inq(n, false);
+  int n = L.size();
+  vector <int> D(n, inf), Q(n), F(n), parent(n, -1), edge(n);
+  vector <bool> inq(n, false);
 
-    int fr = 0, bk = 0;
-    F[s] = inf;
-    D[s] = 0;
-    Q[bk % n] = s;
-    bk ++;
-    inq[s] = true;
+  int fr = 0, bk = 0;
+  F[s] = inf;
+  D[s] = 0;
+  Q[bk % n] = s;
+  bk ++;
+  inq[s] = true;
 
-    while (fr < bk) {
-        int u = Q[fr % n];
-        fr ++;
-        inq[u] = false;
+  while (fr < bk) {
+    int u = Q[fr % n];
+    fr ++;
+    inq[u] = false;
 
-        for (int e = L[u]; e != -1; e = pre[e]) {
-            if (flow[e] < cap[e]) {
-                int v = to[e];
-                int w = cost[e];
-                if (D[v] > D[u] + w) {
-                    D[v] = D[u] + w;
-                    F[v] = min(F[u], cap[e]-flow[e]);
-                    edge[v] = e;
-                    parent[v] = u;
-                    if (!inq[v]) {
-                        Q[bk % n] = v;
-                        bk ++;
-                        inq[v] = true;
-                    }
-                }
-            }
+    for (int e = L[u]; e != -1; e = pre[e]) {
+      if (flow[e] < cap[e]) {
+        int v = to[e];
+        int w = cost[e];
+        if (D[v] > D[u] + w) {
+          D[v] = D[u] + w;
+          F[v] = min(F[u], cap[e]-flow[e]);
+          edge[v] = e;
+          parent[v] = u;
+          if (!inq[v]) {
+            Q[bk % n] = v;
+            bk ++;
+            inq[v] = true;
+          }
         }
+      }
     }
+  }
 
-    if (parent[t] == -1) {
-        return false;
-    }
+  if (parent[t] == -1) {
+    return false;
+  }
 
-    int g = t;
-    while (g != s) {
-        flow[edge[g]] += F[t];
-        flow[edge[g]^1] -= F[t];
-        g = parent[g];
-    }
-    ans.first += F[t];
-    ans.second += D[t];
-    return true;
+  int g = t;
+  while (g != s) {
+    flow[edge[g]] += F[t];
+    flow[edge[g]^1] -= F[t];
+    g = parent[g];
+  }
+  ans.first += F[t];
+  ans.second += D[t];
+  return true;
 }
 
 fc mcmf(int s, int t) {
-    fc ret(0, 0);
-    while (augment(s, t, ret)); 
-    return ret;
+  fc ret(0, 0);
+  while (augment(s, t, ret)); 
+  return ret;
 }
